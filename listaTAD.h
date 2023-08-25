d42ea5fa35831f32c234ffd2e6bf33cd835e3340
Richymelo <castroricardojimenez@gmail.com>
2023-08-25 16:44:33 -0600

    listaTAD

---
+++ listaTAD.h
@@ -0,0 +1,47 @@
+/*
+Se eligio la lista doble enlazada para implementar el programa.
+*/
+//Funciones:
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+
+string agregar (info elem){ 
+//agrega un nuevo item en la lista.
+return 0
+}
+string insertar(info elem, int posicion);{
+    //inserta un nuevo item en una posicion especifica de la lista.
+    return 0
+}
+
+Bool esvacia(); {
+    //determina si la lista esta vacia o no.
+}
+int lenght(); {
+    //indica cual es el largo de la lista.
+    return 0
+}
+Info buscarElem(string elem); {
+    //busca el elemento indicado.
+    return 0
+}
+string remove(Info elem); {
+    //elimina el elemento indicado.
+    return 0
+}
+int getDisplay(int posicion); {
+    //obtiene el item en la poscicion
+    return 0
+}
+string display(); {
+    //obtiene todos los items
+    return 0
+}
+string getNext(); {
+    //obtiene el siguiente elemento en la lista.
+    return 0
+}
+int main {
+    
+}

990af9ae92c5b1995f8cfb6a93e8fba1df59250e
Richymelo <castroricardojimenez@gmail.com>
2023-08-25 16:46:31 -0600

    metodos

---
+++ metodos.h
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+//Funciones
+string getTop5(){
+    //getdisplay();
+    //obtiene los 5 items con mas relevancia
+    return 0
+}
+string getAll(){
+    //diplay();
+    //obtiene todos los items
+    return 0
+}
+string mostrar(string elem){
+    //getNext();
+    //obtiene el siguiente item
+    return 0
+}
+string borrar(string elem){
+    //getNext();
+    //remove(elem);
+}
+string reordenar(info,int nPosicion); {
+    //buscarElem(info);
+    //borrar(info);
+    //insert(info, nPosicion);
+    //reordena un item moviendolo de la posicion donde esta a una nueva.
+    return 0
+ }

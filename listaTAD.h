b498e56badd5ac59d5e52008cf0ddbcb484b78bb
Richymelo <castroricardojimenez@gmail.com>
2023-09-03 21:00:30 -0600

    Caso1-preliminar2

--- listaTAD.h
+++ listaTAD.h
@@ -6,42 +6,113 @@ Se eligio la lista doble enlazada para implementar el programa.
 #include <string>
 #include <bits/stdc++.h>
 
-string agregar (info elem){ 
-//agrega un nuevo item en la lista.
-return 0
+using namespace std;
+
+struct Node {
+    string data;
+    Node* prev;
+    Node* next;
+};
+Node* agregar (string data){ 
+    Node* nNode=new Node();
+    nNode->data=data;
+    nNode->prev=NULL;
+    nNode->next=NULL;
+    return nNode;
 }
-string insertar(info elem, int posicion);{
+void insertar(Node** head, string data, int posicion);{
     //inserta un nuevo item en una posicion especifica de la lista.
-    return 0
+    if (posicion<0 || posicion>lenght(*head)) {
+        cout << "Error: posicion invalida" << endl;
+        return;
+    }
+    Node* nNode=agregar(data);
+    if (posicion==0) {
+        nNode->next= *head;
+        *head = nNode;
+    } 
+    else {
+        Node* temp= *head;
+        for (int i=0; i < posicion-1; i++) {
+            temp=temp->next;
+        }
+        nNode->next=temp->next;
+        temp->next=nNode;
+    }
 }
 
-Bool esvacia(); {
+Bool esvacia(Node* head); {
     //determina si la lista esta vacia o no.
+    return head==NULL;
 }
-int lenght(); {
-    //indica cual es el largo de la lista.
-    return 0
+int lenght(Node*head) {
+    int cont = 0;
+    while (head!=NULL) {
+        cont++;
+        head=head->next;
+    }
+    return cont;
 }
-Info buscarElem(string elem); {
+int buscarElem(Node* head, string elem); {
     //busca el elemento indicado.
-    return 0
+        while (head!=NULL) {
+        if (head->data==elem) {
+            return head->data;
+        }
+        head=head->next;
+    }
+    return -1;//si no encuentra el elemento
 }
-string remove(Info elem); {
+void remove(Node** head, string data); {
     //elimina el elemento indicado.
-    return 0
+    if (esvacia(*head)) {
+        cout << "Error: lista vacia" << endl;
+        return 0;
+    }
+    Node* temp = *head;
+    while (temp != NULL) {
+        if (temp->data == elem) {
+            if (temp == *head) {
+                *head = temp->next;
+                if (*head!=NULL){
+                    (*head)->prev=NULL;
+                }
+            } 
+            else {
+                temp->prev->next = temp->next;
+                
+            }
+            delete temp;
+        }
+        temp = temp->next;
+    }
 }
-int getDisplay(int posicion); {
+int getDisplay(Node* head, int posicion); {
     //obtiene el item en la poscicion
-    return 0
+    Node* temp = *head;
+    for (int i = 0; i < posicion; i++) {
+        temp = temp->next;
+    }
+    return temp->data;
 }
-string display(); {
-    //obtiene todos los items
-    return 0
+
+void display(Node*head); {
+    while(head!=NULL){
+    cout << head->data << " ";
+    head = head->next;
+    }
+    cout<<endl;
 }
-string getNext(); {
+
+Node* getNext(Node* head, int posicion); {
     //obtiene el siguiente elemento en la lista.
-    return 0
-}
-int main {
-    
+    if (posicion < 0 || posicion >= lenght(*head)) {
+        cout << "Error: posicion invalida" << endl;
+        return NULL;
+    }
+    Node* temp = *head;
+    for (int i = 0; i < posicion; i++) {
+        temp = temp->next;
+    }
+    return temp->next;
 }
--- metodos.h
+++ metodos.h
@@ -1,30 +1,69 @@
 #include <iostream>
 #include <string>
 #include <bits/stdc++.h>
+#include <listaTAD.h">
 //Funciones
 string getTop5(){
-    //getdisplay();
-    //obtiene los 5 items con mas relevancia
-    return 0
+    string top = "";
+  for (int i = 0; i < 5; i++) {
+    top += to_string(getDisplay(i)) + "\n";
+  }
+  return top;
 }
 string getAll(){
-    //diplay();
     //obtiene todos los items
-    return 0
+    display();
+    
 }
 string mostrar(string elem){
-    //getNext();
-    //obtiene el siguiente item
+     //obtiene el siguiente item
+    getNext();
     return 0
 }
-string borrar(string elem){
-    //getNext();
-    //remove(elem);
+string borrar(string elem){=
+    remove(elem);
 }
-string reordenar(info,int nPosicion); {
-    //buscarElem(info);
-    //borrar(info);
-    //insert(info, nPosicion);
+string reordenar(Node** head, string elem, int nPosicion); {
     //reordena un item moviendolo de la posicion donde esta a una nueva.
-    return 0
+    if (esvacia(*head)) {
+        cout << "Error: lista vacía" << endl;
+        return;
+    }
+    int encontrado = buscarElem(*head, elem);
+    if (encontrado == -1) {
+        cout << "Error: elemento no encontrado" << endl;
+        return;
+    }
+    remove(head, elem);
+    insertar(head, elem, nPosicion);
+}
+    
+
+ int main() {
+    Node* head = NULL;
+    insertar(&head, "Manzana", 0);
+    insertar(&head, "Banana", 1);
+    insertar(&head, "Cereza", 2);
+    insertar(&head, "Damasco", 3);
+
+    cout << "Todos los elementos:" << endl;
+    display(head);
+
+    cout << "Los 5 primeros elementos:" << endl;
+    string top5 = getTop5(head);
+    cout << top5;
+
+    cout << "El siguiente elemento después del primero:" << endl;
+    string siguiente = mostrar("Manzana");
+    cout << siguiente << endl;
+
+    cout << "Reordenar 'Cereza' a la posición 1:" << endl;
+    reordenar(&head, "Cereza", 1);
+    display(head);
+
+    cout << "Eliminar 'Banana':" << endl;
+    borrar("Banana");
+    display(head);
+
+    return 0;
  }
